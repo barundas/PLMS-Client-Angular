82216735210d81cd267d4d52e24a4fceb234e222
Barun <barun.das@gmail.com>
2017-11-24 17:10:45 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,13 @@ import { PolicyService } from './policies/policy.service';
   <div>
     <nav class='navbar navbar-default'>
       <div class='container-fluid'>
-        <a class='navbar-brand'>{{pageTitle}}</a>
+        <a class='navbar-brand'>{{pageTitle}}</a><br>
         <ul class='nav navbar-nav'>
           <li><a [routerLink]="['/welcome']">Home</a></li>
           <li><a [routerLink]="['/policies']">Policy List</a></li>
+          <li><a [routerLink]="['/welcome']">Register</a></li>
+          <li><a [routerLink]="['/policies']">About Us</a></li>
+          <li><a href="mailto:barun.das@gmail.com?subject=Need Information">Contact Us</a></li>
         </ul>
       </div>
     </nav>
